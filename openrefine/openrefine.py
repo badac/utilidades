cde60a2f1d6c7552d54d7317807b9e82f388f448
Camilo Martinez <cmart@decolector.net>
2022-07-15 10:47:35 -0500

    Código para generar ruta recursiva desde código
    Código para generar una ruta recursiva a partir de un código del BADAC. Se pone en snippets de OpenRefine, pero sirve en python en general

--- openrefine/openrefine.py
+++ openrefine/openrefine.py
@@ -5,8 +5,24 @@
 
 return value.replace(" ","")
 
-## Concatenar dos columnas
+## Concatenar dos columnas ##
 ## Esto también puede ser hecho con "Join Columns"
 
 value = cells['Column 1']['value'] +  cells['Column 2']['value']
 return value
+
+## Generar ruta a partir de codigo ##
+#un codigo cualquiera
+cod = 'ERV_M_CA22_DOC007'
+#lo dividimos en sus partes
+parts = cod.split('_')
+
+#para generar el nombre de cada carpeta, unimos con un '_' 
+#cada parte con las partes anteriores de la lista 
+#luego se pega cada nombre de carpeta con in /
+#para generar ruta recursiva a partir de codigo
+
+path = "/".join(["_".join(parts[:index+1]) for index, part in enumerate(parts)])
+
+print(path)
+#Imprime: 'ERV/ERV_M/ERV_M_CA22/ERV_M_CA22_DOC007'
